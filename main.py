c1d5fbf61e0cf8fb5d20131f1bee5fec4a7689e2
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-09-23 10:56:35 -0500

    Added player/enemy collision to end the game and added start menu

--- main.py
+++ main.py
@@ -33,34 +33,65 @@ walking_enemy_speed = 5
 font = pygame.font.Font("fonts/Modenine-2OPd.ttf", 40)
 score_surf = font.render("SCORE: 0", True, "Black")
 score_rect = score_surf.get_rect(center = (125, 50))
+start_game_surf = font.render("Press 'Space' to Start", True, "Black")
+start_game_rect = start_game_surf.get_rect(center = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+
+in_start_menu = True
+game_playing = False
+in_game_over_menu = False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom == background_surf.get_height():
-                player_gravity -= 15
 
-    screen.fill((0, 0, 0))
+        if in_start_menu:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                in_start_menu = False
+                game_playing = True
 
-    screen.blit(background_surf, background_rect)
-    screen.blit(ground_surf, ground_rect)
+        if game_playing:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom == background_surf.get_height():
+                    player_gravity -= 15
+        elif in_game_over_menu:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                walking_enemy_rect.left = SCREEN_WIDTH + 20
+                game_playing = True
 
-    player_gravity += 0.5
-    player_rect.y += player_gravity
-    if player_rect.bottom >= background_surf.get_height():
-        player_rect.bottom = background_surf.get_height()
-        player_gravity = 0
-    screen.blit(player_surf, player_rect)
+    if in_start_menu:
+        screen.fill((0, 0, 0))
 
-    walking_enemy_rect.x -= walking_enemy_speed
-    if walking_enemy_rect.right < -20:
-        walking_enemy_rect.left = SCREEN_WIDTH + 20
-    screen.blit(walking_enemy_surf, walking_enemy_rect)
-    
-    screen.blit(score_surf, score_rect)
+        screen.blit(background_surf, background_rect)
+        screen.blit(ground_surf, ground_rect)
+        screen.blit(start_game_surf, start_game_rect)
+
+    if game_playing:
+        screen.fill((0, 0, 0))
+
+        screen.blit(background_surf, background_rect)
+        screen.blit(ground_surf, ground_rect)
+
+        player_gravity += 0.5
+        player_rect.y += player_gravity
+        if player_rect.bottom >= background_surf.get_height():
+            player_rect.bottom = background_surf.get_height()
+            player_gravity = 0
+        screen.blit(player_surf, player_rect)
+
+        walking_enemy_rect.x -= walking_enemy_speed
+        if walking_enemy_rect.right < -20:
+            walking_enemy_rect.left = SCREEN_WIDTH + 20
+        screen.blit(walking_enemy_surf, walking_enemy_rect)
+        
+        screen.blit(score_surf, score_rect)
+
+        if player_rect.colliderect(walking_enemy_rect):
+            game_playing = False
+            in_game_over_menu = True
+    elif in_game_over_menu:
+        screen.fill("Blue")
 
     pygame.display.update()
     clock.tick(60)
