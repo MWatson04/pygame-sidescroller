20d053f2f7a94d3941261995167e561f05f4e43e
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-09-23 16:07:53 -0500

    Adjusted flying enemy height and speed

--- enemy.py
+++ enemy.py
@@ -14,5 +14,5 @@ class FlyingEnemy:
     def __init__(self):
         self.flying_enemy_surf = display.pygame.transform.scale_by(
             display.pygame.image.load("images/Tiles/Characters/second_enemy.png").convert_alpha(), 3)
-        self.flying_enemy_rect = self.flying_enemy_surf.get_rect(bottomright = (display_obj.SCREEN_WIDTH + 200, 250))
-        self.flying_enemy_speed = 4
+        self.flying_enemy_rect = self.flying_enemy_surf.get_rect(bottomright = (display_obj.SCREEN_WIDTH + 200, 225))
+        self.flying_enemy_speed = 3.5
